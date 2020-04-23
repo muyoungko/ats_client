130f188b1bdc7a55ccf1ef5bda8dfbd7eeba068c
고무영 <muyoungko@sk.com>
2020-04-23 13:40:14 +0900

    a

--- src/api/api.js
+++ src/api/api.js
@@ -24,63 +24,80 @@ const test = async (member_no, token, json) => {
     const test_session_id = json.test_session_id;
     req(token, `/test_session_pop/${test_session_id}`, (json)=>{
         if(json.r){
-            var code = json.data.code
+            try{
+                var code = json.data.code
 
-            //test_session_id
-            code = code.replace(/__TOKEN__/gi, token)
-            .replace(/__API_HOST__/gi, config.api_host)
-            .replace(/__TEST_SESSION_ID__/gi, test_session_id); 
+                //test_session_id
+                code = code.replace(/__TOKEN__/gi, token)
+                .replace(/__API_HOST__/gi, config.api_host)
+                .replace(/__TEST_SESSION_ID__/gi, test_session_id); 
 
-            /*
-                파이썬2에 자꾸 설치되서 python3에서 못찾음 다음과 같이 설치해야한다.
-                python3 -m pip install Appium-Python-Client
-            */
-            let options = {
-                mode: 'text',
-                pythonOptions: ['-u'], // get print results in real-time
-                args: ['value1', 'value2', 'value3']
-            };
-
-            console.log('script runnging...');
-            const pyshell = PythonShell.runString(code, options, function (err, results) {
-                var body = {};
-                if (err) {
-                    console.log('-------------------------------err------------------------------');
-                    console.log(Object.keys(err));
-                    // console.log('executable', err.executable);
-                    // console.log('options', err.options);
-                    // console.log('script', err.script);
-                    // console.log('args', err.args);
-                    body.message = err.message;
-                    body.traceback = err.traceback;
-                } else {
-                    //코드가 syntex오류가 실패한다면 실패 할 때까지 출력된 로그가 results에 수집되지 않는다.
-                    //TODO : 실패 로그를 수집해야한다.
-                    body.results = results;
-                    console.log('-------------------------------success------------------------------');
-                    console.log('--------------------------results-----------------------------------');
-                    console.log(results)
+                /*
+                    파이썬2에 자꾸 설치되서 python3에서 못찾음 다음과 같이 설치해야한다.
+                    python3 -m pip install Appium-Python-Client
+                */
+                let options = {
+                    mode: 'text',
+                    pythonOptions: ['-u'], // get print results in real-time
+                    args: ['value1', 'value2', 'value3']
                 };
+
+                console.log('script runnging...');
+
+                const filename = `test_${test_session_id}.py`; 
+                fs = require('fs');
+                fs.writeFileSync(filename, code, 'utf8')            
                 
+                let pyshell = new PythonShell(filename, options);
+                var results = [];
+                var traceback = [];
+                pyshell.on('message', function (message) {
+                    // received a message sent from the Python script (a simple "print" statement)
+                    console.log(' result ', message);
+                    results.push(message);
+                    
+                    request({
+                        url: config.api_host + `/test_session_log/${test_session_id}`,
+                        headers: {
+                            'x-access-token': token,
+                            'content-type':'application/json'
+                        },
+                        json:true,
+                        method: 'POST',
+                        body:{
+                            log : message
+                        },
+                    }, async (err, resp, body) => {
 
-                request({
-                    url: config.api_host + `/test_session_complete/${test_session_id}`,
-                    headers: {
-                        'x-access-token': token,
-                        'content-type':'application/json'
-                    },
-                    json:true,
-                    method: 'POST',
-                    body:body,
-                },
-                async (err, resp, body) => {
-                });
-            });
+                    });
+                })
+                .on('stderr', function (stderr) {
+                    console.log(' stderr ', stderr);
+                    traceback.push(stderr);
+                })
+                .end(function (err) {
+                    console.log(' end ', err);
+                    console.log('python finished');
+                    request({
+                        url: config.api_host + `/test_session_complete/${test_session_id}`,
+                        headers: {
+                            'x-access-token': token,
+                            'content-type':'application/json'
+                        },
+                        json:true,
+                        method: 'POST',
+                        body:{
+                            success : traceback.length == 0,
+                            results:results, traceback:traceback
+                        },
+                    }, async (err, resp, body) => {
 
-            pyshell.on('message', function (message) {
-                // received a message sent from the Python script (a simple "print" statement)
-                console.log(message);
-              });
+                    });
+                });;
+            
+            }catch(e){
+                console.log(' try - catch ', e);
+            }
         } else {
             console.log('Test session is already poped');
         }
---
+++ src/sample/py.js
@@ -0,0 +1,117 @@
+const {PythonShell} = require('python-shell');
+
+
+let options = {
+    mode: 'text',
+    pythonOptions: ['-u'], // get print results in real-time
+    args: ['value1', 'value2', 'value3']
+};
+
+const code = 
+
+`
+import sys
+
+def console() :
+    print('console : ')
+    while True:
+        input = sys.stdin.readline()
+        if input.startswith('exit') :
+            break
+        exec(input)
+
+print('start new ')
+a = 1 + 1
+print(a)
+
+console()
+
+`
+
+const sleep = (ms) => {
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
+
+
+const start = async () =>{
+    console.log('script runnging...');
+
+    const filename = `var/f.py`; 
+    fs = require('fs');
+    fs.writeFileSync(filename, code, 'utf8')
+
+    let pyshell = new PythonShell(filename, options);
+    // const pyshell = PythonShell.runString(code, options, function (err, results) {
+    //     var body = {};
+    //     if (err) {
+    //         console.log('-------------------------------err------------------------------');
+    //         console.log(Object.keys(err));
+    //         console.log(err);
+    //         // console.log('executable', err.executable);
+    //         // console.log('options', err.options);
+    //         // console.log('script', err.script);
+    //         // console.log('args', err.args);
+    //         body.message = err.message;
+    //         body.traceback = err.traceback;
+    //     } else {
+    //         //코드가 syntex오류가 실패한다면 실패 할 때까지 출력된 로그가 results에 수집되지 않는다.
+    //         //TODO : 실패 로그를 수집해야한다.
+    //         body.results = results;
+    //         console.log('-------------------------------success------------------------------');
+    //         console.log('--------------------------results-----------------------------------');
+    //         console.log(results)
+    //     };
+        
+    // });
+
+    pyshell.on('message', function (message) {
+        // received a message sent from the Python script (a simple "print" statement)
+        console.log(' - ', message);
+    });
+
+    pyshell.on('stdout', function (stdout) {
+        console.log(' stdout ', stdout);
+    });
+
+    pyshell.on('stderr', function (stderr) {
+        console.log(' stderr ', stderr);
+    });
+
+    pyshell.on('close', function (stderr) {
+        console.log(' close ');
+    });
+
+    await sleep(3000)
+
+    pyshell.send(`print('count down')`)
+
+    await sleep(1000)
+    pyshell.send(`print('5')`)
+    await sleep(1000)
+    pyshell.send(`print('4')`)
+    await sleep(1000)
+    pyshell.send(`print('3')`)
+    await sleep(1000)
+    pyshell.send(`print('2')`)
+    await sleep(1000)
+    pyshell.send(`print('1')`)
+    await sleep(1000)
+    pyshell.send(`print('0')`)
+    await sleep(1000)
+    pyshell.send(`exit`)
+
+    await sleep(1000)
+    // pyshell.end(function (err,code,signal) {
+    //     if (err) throw err;
+    //     console.log('The exit code was: ' + code);
+    //     console.log('The exit signal was: ' + signal);
+    //     console.log('finished');
+    // });
+
+    console.log('------end------')
+}
+
+
+start()
